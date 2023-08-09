e05c56cb6f7073e53fac2074354ebacf0a4a159c
AYehia0 <ahmedyehia611@gmail.com>
2023-08-09 17:32:06 +0300

    Testing GetAccountAPI with Table Test Driven

--- api/account_test.go
+++ api/account_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -19,34 +20,94 @@ import (
 func TestGetAccountAPI(t *testing.T) {
 	account := getRandomAccount()
 
-	// create a new mock controller to be able to use/build the mock's stubs
-	controller := gomock.NewController(t)
+	testCases := []struct {
+		testName   string
+		accountId  int64
+		buildStubs func(store *mockdb.MockStore)
+		checkResp  func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			testName:  "OK",
+			accountId: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountById(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(account, nil)
+			},
+			checkResp: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchAccount(t, recorder.Body, account)
 
-	// invoked once, check to see if all methods that were expected to be called were called.
-	defer controller.Finish()
+			},
+		},
+		{
+			testName:  "NotFound",
+			accountId: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountById(gomock.Any(), gomock.Eq(account.ID)).
+					Times(1).
+					Return(db.Account{}, sql.ErrNoRows)
+			},
+			checkResp: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			// since the validation happens before calling any function inside the API
+			testName:  "BadRequest",
+			accountId: -1,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountById(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			checkResp: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			testName:  "InternalError",
+			accountId: account.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccountById(gomock.Any(), gomock.Any()).
+					Times(1).
+					Return(db.Account{}, sql.ErrConnDone)
+			},
+			checkResp: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
 
-	store := mockdb.NewMockStore(controller)
+	for _, testCase := range testCases {
+		t.Run(testCase.testName, func(t *testing.T) {
+			// create a new mock controller to be able to use/build the mock's stubs
+			controller := gomock.NewController(t)
 
-	store.EXPECT().
-		GetAccountById(gomock.Any(), gomock.Eq(account.ID)).
-		Times(1).
-		Return(account, nil)
+			defer controller.Finish()
 
-	// start a server and handle requests using httpserver
-	server := NewServer(store)
+			store := mockdb.NewMockStore(controller)
 
-	// we don't have to start a real server, instead we can use the recorder to catch/send req/res
-	recorder := httptest.NewRecorder()
+			testCase.buildStubs(store)
 
-	// TODO: any change in the paths won't be reflected here
-	urlPath := fmt.Sprintf("/accounts/%d", account.ID)
-	req := httptest.NewRequest(http.MethodGet, urlPath, nil)
+			// start a server and handle requests using httpserver
+			server := NewServer(store)
 
-	// send the request and capture to the recorder
-	server.router.ServeHTTP(recorder, req)
+			// we don't have to start a real server, instead we can use the recorder to catch/send req/res
+			recorder := httptest.NewRecorder()
 
-	require.Equal(t, http.StatusOK, recorder.Code)
-	requireBodyMatchAccount(t, recorder.Body, account)
+			// TODO: any change in the paths won't be reflected here
+			urlPath := fmt.Sprintf("/accounts/%d", testCase.accountId)
+			req := httptest.NewRequest(http.MethodGet, urlPath, nil)
+
+			// send the request and capture to the recorder
+			server.router.ServeHTTP(recorder, req)
+			testCase.checkResp(t, recorder)
+		})
+	}
 }
 
 func getRandomAccount() db.Account {
