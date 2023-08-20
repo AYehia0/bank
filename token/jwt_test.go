8c36d7f5874f9af4ec2a9cb763ce11842c93e3a1
AYehia0 <ahmedyehia611@gmail.com>
2023-08-20 12:16:21 +0000

    Check the UUID of the token

--- token/jwt_test.go
+++ token/jwt_test.go
@@ -31,4 +31,48 @@ func TestValidateToken(t *testing.T) {
 	require.Equal(t, payload.Username, username)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 	require.WithinDuration(t, createdAt, payload.CreatedAt, time.Second)
+	
+	// check if the UUID was given
+	require.NotZero(t, payload.Id)
+}
+
+func TestInvalidToken(t *testing.T) {
+	t.Run("TokenExpired", func(t *testing.T) {
+		secret := utils.RandomString(32)
+		creator, err := NewJWTCreator(secret)
+
+		require.NoError(t, err)
+
+		username := utils.GetRandomOwnerName()
+		token, err := creator.Create(
+			username,
+			-time.Minute,
+		)
+		require.NoError(t, err)
+		require.NotEmpty(t, token)
+
+		payload, err := creator.Verify(token)
+		require.Error(t, err)
+		require.ErrorIs(t, err, TokenExpiredError)
+		require.Nil(t, payload)
+	})
+
+	t.Run("InvalidAlgo", func(t *testing.T) {
+
+		payload, err := NewPayload(utils.GetRandomOwnerName(), time.Minute)
+		require.NoError(t, err)
+
+		// create a new token
+		token := jwt.NewWithClaims(jwt.SigningMethodNone, payload)
+
+		// sign/create the token
+		fToken, err := token.SignedString(jwt.UnsafeAllowNoneSignatureType)
+		require.NoError(t, err)
+
+		// trying to verify that token
+		creator, err := NewJWTCreator(utils.RandomString(32))
+		payload, err = creator.Verify(fToken)
+		require.Error(t, err)
+		require.ErrorIs(t, err, TokenInvalidError)
+	})
 }
